4b88555155256ac61e72a0161fd1c6c2da594c17
wyattgust <122560200+wyattgust@users.noreply.github.com>
2023-01-13 15:06:50 -0500

    Add files via upload

---
+++ adventure_game_format.py
@@ -0,0 +1,144 @@
+print ('Hi there, welcome to the game of life.')
+print ('Options for your journey will appear in ALL CAPS.')
+name = input('What is your name? ')
+print (f'Awesome {name.title()}, lets get started! ')
+print ("You've just woke up the light burns your eyes as it pierces throught the blinds. \nYou've been asleep for three days. \nAs you blink the jarring sunshine away you see in the near distance a lamp that wsnt there before. \nInspect?")
+ans1 = input ("YES/NO ")
+
+if ans1.lower() == 'yes':
+    print ('You notice that this lamp is no ordinary lamp. It seems to be one of those \n Greek pots. A voice from a lecture you heard in college, \n"this image here depicts Achillies playing dice." \n You think, how the heck did that get here? ')
+    print ('Immediately as you go to pick it up you accidentaly fumbed it and witness it smash to the ground into a million pieces. ')
+    print('You hear laughing in the next room. ')
+    
+    ans2 = input ('WALK TOWARDS the laughter or RUN AWAY?')
+
+    if ans2.lower() == 'walk towards' or ans2.lower() == 'walk':
+        print ('As you get closer you see two dudes talking something about a heist.' "Hey you're awake! Finally." 'they say.')
+        print ('You ask:')
+    
+        ans3 = input ('"WHO ARE YOU" \n"WHAT HAPPENED? " \n"WHO AM I?"')
+        if ans3.lower() == 'who are you' or ans3.lower() == 'who are you?':
+            print("Buddy whadya mean. We're art robbers my boy.")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        elif ans3.lower() == 'what happened' or ans3.lower() == 'what happened?':
+            print("Buddy whadya mean. We barely made it out alive")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        elif ans3.lower() == 'who am i' or ans3.lower () == 'who am i?':
+            print("Buddy whadya mean. We're art robbers, you just helped in the most notorius theft of our century. You stole the Mona lisa!")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        else:
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+    
+    
+    elif ans2.lower() == 'run away' or ans2.lower() == 'run':
+        print ('You ran through the fron doorway and see a beautiful black Lincoln parked at the end of the steps littered \nwith bullet holes. The two men walk up behind you. \nThey put their hand on each of your shoulders. ')
+        
+        print ("Hey you're awake finally" 'they say.')
+        
+        print ('You ask:')
+        
+        ans3 = input ('"WHO ARE YOU" \n"WHAT HAPPENED? " \n"WHO AM I?"')
+        if ans3.lower() == 'who are you' or ans3.lower() == 'who are you?':
+            print("Buddy whadya mean. We're art robbers my boy.")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        elif ans3.lower() == 'what happened' or ans3.lower() == 'what happened?':
+            print("Buddy whadya mean. We barely made it out alive")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        elif ans3.lower() == 'who am i' or ans3.lower () == 'who am i?':
+            print ("Buddy whadya mean. We're art robbers, you just helped in the most notorius theft of our century. You stole the Mona lisa! ")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        else:
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        
+    else:
+        print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+
+
+elif ans1.lower() == 'no': 
+    print ('You look to your right hand and notice a beautiful silver daggar with an ornate blade with an even more ornate golden hilt.')
+    print ("A couple of swings prove it to be well crafted. The fact that it's here in your posession is a mystery.")
+    print('You hear laughing in the next room. ')
+    
+    ans2 = input ('WALK TOWARDS the laughter or RUN AWAY?')
+
+    if ans2.lower() == 'walk towards' or ans2.lower() == 'walk':
+        print ('As you get closer you see two dudes talking something about a heist.' "Hey you're awake! Finally." 'they say.')
+        print ('As you get closer you see two dudes talking something about a heist.' "Hey you're awake! Finally." 'they say.')
+        print ('You ask:')
+    
+        ans3 = input ('"WHO ARE YOU" \n"WHAT HAPPENED? " \n"WHO AM I?"')
+        if ans3.lower() == 'who are you' or ans3.lower() == 'who are you?':
+            print("Buddy whadya mean. We're art robbers my boy.")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        elif ans3.lower() == 'what happened' or ans3.lower() == 'what happened?':
+            print("Buddy whadya mean. We barely made it out alive")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        elif ans3.lower() == 'who am i' or ans3.lower () == 'who am i?':
+            print("Buddy whadya mean. We're art robbers, you just helped in the most notorius theft of our century. You stole the Mona lisa!")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        else:
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+
+    elif ans2.lower() == 'run away' or ans2.lower() == 'run':
+        
+        print ('You ran through the fron doorway and see a beautiful black Lincoln parked at the end of the steps littered \nwith bullet holes. The two men walk up behind you. \nThey put their hand on each of your shoulders. ')
+        print ("Hey you're awake finally" 'they say.')
+        print ('You ask:')
+        
+        ans3 = input ('"WHO ARE YOU" \n"WHAT HAPPENED? " \n"WHO AM I?"')
+        if ans3.lower() == 'who are you' or ans3.lower() == 'who are you?':
+            
+            print("Buddy whadya mean. We're art robbers my boy.")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        elif ans3.lower() == 'what happened' or ans3.lower() == 'what happened?':
+            
+            print("Buddy whadya mean. We barely made it out alive")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        elif ans3.lower() == 'who am i' or ans3.lower () == 'who am i?':
+            
+            print ("Buddy whadya mean. We're art robbers, you just helped in the most notorius theft of our century. You stole the Mona lisa! ")
+            print('Immmediately this all starts making sense as the memory of the robbery flashes through your mind.')
+            print("It's just too bad now that were done, because that means we're done with you. ")
+            print('...Bang!!!')
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        else:
+            print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+        
+    else:
+        print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
+
+else:
+    print('You notice a hole in your chest, you see blood pouring from it. You gasp. Then you die leaving this world forever. \nGAME OVER')
